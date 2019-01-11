a46df94c2b9eaff72cc5061a4a2db67cba7c2adb
Charles Buckley <charlesbuckley122@gmail.com>
2019-01-11 10:06:44 -0500

    updated to v2.0

---
+++ calculator-2.js
@@ -0,0 +1,45 @@
+//Calculate Tip
+function calculateTip() {
+    var billInput = document.getElementById("billInput").value;
+    var smallTip = .15;
+    var medTip = .18;
+    var largeTip = .2;
+
+
+    //Make sure user types in a number
+    if (billInput === "" || billInput === "0") {
+        console.log("Please enter a value");
+        document.getElementById("tipAmount").value = "Please enter an amount";
+        return;
+    }
+
+   
+    //Calculate tip
+    var total;
+
+    if (document.getElementById('selector').value == 'small'){
+        total = billInput * smallTip;
+    } else if (document.getElementById('selector').value == 'medium'){
+        total = billInput * medTip;
+    } else if (document.getElementById('selector').value == 'large'){
+        total = billInput * largeTip;
+
+    }
+
+
+
+    //round to two decimal places
+    total = Math.round(total * 100) / 100;
+    //next line allows two digits after decimal point
+    total = total.toFixed(2);
+
+    //Display the tip
+    document.getElementById("tipAmount").value = total;
+
+
+}
+
+document.getElementById('button').onclick = function () {
+    calculateTip();
+
+};
